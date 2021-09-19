bfa28ab7f1670318d3506da06e9880be0453c0a4
antoniomartinszup <antonio.martins@zup.com.br>
2021-09-19 15:33:54 -0300

    add testes

--- src/integracao/java/br/com/zupacademy/antonio/mercadolivre/controller/CategoriaControllerTest.java
+++ src/integracao/java/br/com/zupacademy/antonio/mercadolivre/controller/CategoriaControllerTest.java
@@ -1,14 +1,17 @@
 package br.com.zupacademy.antonio.mercadolivre.controller;
 
 import br.com.zupacademy.antonio.mercadolivre.controller.form.CategoriaForm;
+import br.com.zupacademy.antonio.mercadolivre.model.Categoria;
 import br.com.zupacademy.antonio.mercadolivre.repository.CategoriaRepository;
 import com.google.gson.Gson;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -34,11 +37,60 @@ class CategoriaControllerTest {
     }
 
     @Test
+    @DisplayName("Tentativa de Cadastrar Categoria sem Permissao")
     public void tentativaDeCadastrarCategoriaSemTerPermissao() throws Exception {
         CategoriaForm categoriaForm = new CategoriaForm("Tecnologia", null);
         mockMvc.perform(post("/categorias")
                         .content(gson.toJson(categoriaForm))
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().is(403));
+    }
+
+    @Test
+    @WithMockUser
+    @DisplayName("Cadastra Categoria com sucesso")
+    public void cadastraSucesso() throws Exception {
+        CategoriaForm categoriaForm = new CategoriaForm("Tecnologia", null);
+        mockMvc.perform(post("/categorias")
+                        .content(gson.toJson(categoriaForm))
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
+
+//    @Test
+//    @WithMockUser
+//    @DisplayName("Cadastra Categoria e Categoria Mae com sucesso")
+//    public void cadastraCategoriaMaeSucesso() throws Exception {
+//        Categoria categoriaMae = new Categoria("Tecnologia");
+//        categoriaRepository.save(categoriaMae);
+//        CategoriaForm categoriaForm = new CategoriaForm("Celular", 1L);
+//        mockMvc.perform(post("/categorias")
+//                        .content(gson.toJson(categoriaForm))
+//                        .contentType(MediaType.APPLICATION_JSON))
+//                .andExpect(status().isOk());
+//    }
+
+    @Test
+    @WithMockUser
+    @DisplayName("Falha no cadastro de Categoria atributo nome duplicado")
+    public void falhaNoCadastroCategoriaNomeDuplicado() throws Exception {
+        Categoria categoriaMae = new Categoria("Tecnologia");
+        categoriaRepository.save(categoriaMae);
+        CategoriaForm categoriaForm = new CategoriaForm("Tecnologia", null);
+        mockMvc.perform(post("/categorias")
+                        .content(gson.toJson(categoriaForm))
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    @WithMockUser
+    @DisplayName("Falha no cadastro de Categoria atributo nome")
+    public void falhaNoCadastroCategoriaNome() throws Exception {
+        CategoriaForm categoriaForm = new CategoriaForm("", null);
+        mockMvc.perform(post("/categorias")
+                        .content(gson.toJson(categoriaForm))
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest());
     }
 }
--- src/integracao/java/br/com/zupacademy/antonio/mercadolivre/controller/UsuarioControllerTest.java
+++ src/integracao/java/br/com/zupacademy/antonio/mercadolivre/controller/UsuarioControllerTest.java
@@ -1,9 +1,12 @@
 package br.com.zupacademy.antonio.mercadolivre.controller;
 
 import br.com.zupacademy.antonio.mercadolivre.controller.form.UsuarioForm;
+import br.com.zupacademy.antonio.mercadolivre.model.Usuario;
 import br.com.zupacademy.antonio.mercadolivre.repository.UsuarioRepository;
+import br.com.zupacademy.antonio.mercadolivre.security.SenhaLimpa;
 import com.google.gson.Gson;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -12,12 +15,18 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
+import javax.transaction.Transactional;
+
+import java.util.Locale;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@Transactional
 @ActiveProfiles("test")
 public class UsuarioControllerTest {
 
@@ -35,6 +44,7 @@ public class UsuarioControllerTest {
     }
 
     @Test
+    @DisplayName("Cadastra Usuario com sucesso")
     public void cadastrarUsuarioComDadosSucesso() throws Exception {
         UsuarioForm usuarioForm = new UsuarioForm("zepa@email.com", "123456");
         mockMvc.perform(post("/usuarios")
@@ -44,20 +54,49 @@ public class UsuarioControllerTest {
     }
 
     @Test
-    public void cadastrarUsuarioComFalhaFormatoEmail() throws Exception {
+    @DisplayName("Falha no cadastro do Usuario atributo email formato invalido")
+    public void falhaNoCadastroUsuarioFormatoEmail() throws Exception {
         UsuarioForm usuarioForm = new UsuarioForm("zepa", "123456");
         mockMvc.perform(post("/usuarios")
+                        .locale(new Locale("pt", "BR"))
                         .content(gson.toJson(usuarioForm))
                         .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isBadRequest());
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$[0].campo").value("login"))
+                .andExpect(jsonPath("$[0].erro").value("deve ser um endereço de e-mail bem formado"))
+                .andReturn().getResponse();
     }
 
     @Test
-    public void cadastrarUsuarioComFalhaFormatoSenha() throws Exception {
+    @DisplayName("Falha no cadastro do Usuario atributo senha formato invalido")
+    public void falhaNoCadastroUsuarioFormatoSenha() throws Exception {
         UsuarioForm usuarioForm = new UsuarioForm("zepa@email.com", "123");
         mockMvc.perform(post("/usuarios")
+                        .locale(new Locale("pt", "BR"))
                         .content(gson.toJson(usuarioForm))
                         .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().is(400));
+                .andExpect(status().is(400))
+                .andExpect(jsonPath("$[0].campo").value("senha"))
+                .andExpect(jsonPath("$[0].erro").value("tamanho deve ser entre 6 e 2147483647"))
+                .andReturn().getResponse();
+    }
+
+    @Test
+    @DisplayName("Falha no cadastro do Usuario atributo email duplicado")
+    public void falhaNoCadastroUsuarioEmailDuplicado() throws Exception {
+
+        Usuario usuarioDuplicado = new Usuario("zepa@email.com", new SenhaLimpa("123456"));
+        usuarioRepository.save(usuarioDuplicado);
+        UsuarioForm usuarioForm = new UsuarioForm("zepa@email.com", "123456");
+
+        mockMvc.perform(post("/usuarios")
+                        .locale(new Locale("pt", "BR"))
+                        .content(gson.toJson(usuarioForm))
+                        .contentType(MediaType.APPLICATION_JSON))
+
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$[0].campo").value("login"))
+                .andExpect(jsonPath("$[0].erro").value("Item já cadastrado"))
+                .andReturn().getResponse();
     }
 }
